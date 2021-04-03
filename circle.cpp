e18251fda893a16e7182c54aa069e99f6e6dcecd
Alireza Farahani <30633987+amfarahani@users.noreply.github.com>
2021-04-03 09:37:06 -0700

    Add file via upload
    A simple program in C++ to fine the area of a circle.

---
+++ circle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//This program prompts user to enter circle raduis
+//Then the circle area is displayed
+
+int main()
+{
+	double radius;
+	cout << "Enter radius\n";
+	//Add code to read user input and store in variable radius
+	cout << "The area of a circle with radius \n";
+	//Calculate the area of the circle
+	return 0;
+}
